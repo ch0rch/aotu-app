4e224efed82be45cc5102511396b199e7c3b4d68
ch0rch <georojas.gs@gmail.com>
2025-01-27 15:55:26 -0300

    new

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { Home, Bell, Package, ShoppingBag } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 export function Sidebar({ className }: React.HTMLAttributes<HTMLDivElement>) {
   return (
