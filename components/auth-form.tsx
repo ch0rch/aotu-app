c3e4e85d31dd14e3d82d4b843f915a4a6a2d81c1
ch0rch <georojas.gs@gmail.com>
2025-01-27 20:41:09 -0300

    asd

--- components/auth-form.tsx
+++ components/auth-form.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -17,7 +16,7 @@ export function AuthForm() {
   const [password, setPassword] = useState("")
   const [authMode, setAuthMode] = useState<AuthMode>("login")
   const { toast } = useToast()
-  const router = useRouter()
+  //const router = useRouter()
 
   async function onSubmit(event: React.FormEvent) {
     event.preventDefault()
