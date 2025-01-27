4b4d3819ceee866a5d18b439deb90817d452bbaa
ch0rch <georojas.gs@gmail.com>
2025-01-27 12:44:45 -0300

    Página de login

---
+++ lib/components/AuthForm.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Icons } from "@/components/ui/icons"
+
+export function AuthForm() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault()
+    setIsLoading(true)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }
+
+  return (
+    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+      <div className="flex flex-col space-y-2 text-center">
+        <Icons.logo className="mx-auto h-6 w-6" />
+        <h1 className="text-2xl font-semibold tracking-tight">Bienvenido a AOTU</h1>
+        <p className="text-sm text-muted-foreground">Ingresa tus credenciales para acceder al sistema</p>
+      </div>
+      <div className={`grid gap-6`}>
+        <form onSubmit={onSubmit}>
+          <div className="grid gap-2">
+            <div className="grid gap-1">
+              <Label className="sr-only" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                id="email"
+                placeholder="nombre@ejemplo.com"
+                type="email"
+                autoCapitalize="none"
+                autoComplete="email"
+                autoCorrect="off"
+                disabled={isLoading}
+              />
+            </div>
+            <div className="grid gap-1">
+              <Label className="sr-only" htmlFor="password">
+                Contraseña
+              </Label>
+              <Input
+                id="password"
+                placeholder="Contraseña"
+                type="password"
+                autoCapitalize="none"
+                autoComplete="current-password"
+                autoCorrect="off"
+                disabled={isLoading}
+              />
+            </div>
+            <Button disabled={isLoading}>
+              {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+              Iniciar sesión
+            </Button>
+          </div>
+        </form>
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">O continúa con</span>
+          </div>
+        </div>
+        <Button variant="outline" type="button" disabled={isLoading}>
+          {isLoading ? (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <Icons.google className="mr-2 h-4 w-4" />
+          )}{" "}
+          Google
+        </Button>
+      </div>
+      <p className="px-8 text-center text-sm text-muted-foreground">
+        Al hacer clic en continuar, aceptas nuestros{" "}
+        <a href="/terms" className="underline underline-offset-4 hover:text-primary">
+          Términos de servicio
+        </a>{" "}
+        y{" "}
+        <a href="/privacy" className="underline underline-offset-4 hover:text-primary">
+          Política de privacidad
+        </a>
+        .
+      </p>
+    </div>
+  )
+}
+
